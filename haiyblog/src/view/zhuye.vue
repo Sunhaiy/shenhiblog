bc2e977a5ed57c0e6ed4c56dcdf412790945ef9c
孫海洋 <s744129991@outlook.com>
2025-01-31 19:52:55 +0800

    写了一点暂时没啥思路

--- haiyblog/src/view/zhuye.vue
+++ haiyblog/src/view/zhuye.vue
@@ -1,9 +1,28 @@
 <template>
-<div id="root">
-    <div id="top"></div>
-    <div id="center"></div>
-    <div id="bottom"></div>
-</div>
+    <div id="root">
+        <div id="top">
+            <div id="topleft">
+                <span id="title">Overview</span>
+                <h2 id="untitle">早上好，孫海洋👋</h2>
+            </div>
+            <div id="topright">
+                <IconField>
+                    <InputIcon class="pi pi-search" />
+                    <InputText v-model="value1" placeholder="Search" />
+                </IconField>
+                <Button icon="pi pi-envelope" iconPos="top" id="btnright" />
+            </div>
+        </div>
+        <div id="center">
+            <div id="lunbo">
+                
+                
+            </div>
+            <div id="jishuzhan"></div>
+            <div id="wenzhang"></div>
+        </div>
+        <div id="bottom"></div>
+    </div>
 </template>
 
 
@@ -11,26 +30,86 @@
 <script setup>
 
 
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
 </script>
 
 
 
 <style scoped>
-#root{
+#root {
+    margin-right: 20px;
     width: 100%;
     height: 100%;
     display: flex;
     flex-direction: column;
     overflow: hidden;
 }
-#top{
+
+#top {
     flex: 1;
+    display: flex;
+
 }
-#center{
-    flex: 1;
-}
-#bottom{
-    flex: 1;
+
+#center {
     
+    flex: 5;
+    display: flex;
+    flex-direction: column;
+}
+
+#bottom {
+    flex: 1;
+
+}
+
+#topleft {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+}
+
+#topright {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: flex-end;
+    align-items: flex-start;
+    flex: 1;
+}
+
+#title {
+    font-size: larger;
+    color: #72646B;
+}
+
+#btnright {
+    margin-left: 10px;
+    
+}
+
+#lunbo {
+    flex: 3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border:1px solid #72646B;
+    border-radius: 10px;
+}
+
+#jishuzhan {
+    flex:1;
+}
+
+#wenzhang {
+    flex: 2;
+}
+#wenzhang,#jishuzhan{
+    border:1px solid #72646B;
+    border-radius: 10px;
+    margin-top: 20px;
 }
 </style>
