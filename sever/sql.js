1203110d1a413f35df83958ef4a66aaf72947b84
孫海洋 <s744129991@outlook.com>
2025-02-27 23:16:07 +0800

    随便gg

--- sever/sql.js
+++ sever/sql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host:'127.0.0.1',
     user:'root',
-    password:'123123123',
+    password:'123123',
     database:'blog',
     port:3306
 })
