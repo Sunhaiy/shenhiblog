ea983637df21fe6067aabc305d3582c2286795fb
孫海洋 <s744129991@outlook.com>
2025-03-02 19:57:45 +0800

    来生页面开发了一半，还有我的页面有想法，评论功能还没有实现

--- haiyblog/src/components/card.vue
+++ haiyblog/src/components/card.vue
@@ -1,10 +1,16 @@
 <template>
-    <div id="card">
+    <div id="card" v-for="i in 10">
         <div id="heard">
-            <img src="" alt="" id="awatar">
-            <span>孫海洋</span>
+            <img src="https://www.primefaces.org/cdn/primevue/images/landing/apps/avatar7.png" alt="" id="awatar">
+
+            <span id="name">孫海洋</span>
         </div>
-        <div id="inner"></div>
+        <hr id="line">
+        <div id="inner">
+            这是正文
+        </div>
+        <span id="time">2025/3/2</span>
+        <span id="weather">🌨下雪天</span>
     </div>
 </template>
 
@@ -21,21 +27,63 @@
 #card{
     display: flex;
     flex-direction: column;
-    width: 900px;
-    height: 200px;
+    width: 500px;
+    margin: 10px;
     background-color: #262626;
     border-radius: 20px;
+    border: 1px solid #4e4d4d;
+    transition: all 0.3s;
+}
+#card:hover{
+    background-color: #2b2b2b;
+    border: 1px solid #696868;
 }
 #heard{
-    
+    display: flex;
+    align-items: center;
+    padding: 20px;
 }
 #inner{
     flex: 1;
+    padding: 20px;
+    font-weight: bold;
+    font-size: large;
 }
 #awatar{
-    width: 50px;
-    height: 50px;
+    margin-right: 10px;
+    width: 40px;
+    height: 40px;
     border-radius: 50%;
-    margin: 20px;
+    
+}
+#name{
+    font-weight: bold;
+    font-size: 15px;
+}
+#line{
+    width: 100%;
+    border: 1px solid #4e4d4d;
+    
+    
+}
+#time{
+    font-weight: bold;
+    
+    font-size: 15px;
+    color: #b3b3b3;
+    align-self: flex-end;
+    justify-content: end;
+    margin-right: 10px;
+    
+}
+#weather{
+    font-weight: bold;
+    padding-top: 2px;
+    padding-bottom:10px ;
+    font-size: 15px;
+    color: #b3b3b3;
+    align-self: flex-end;
+    justify-content: end;
+    margin-right: 10px;
 }
 </style>
--- haiyblog/src/view/laisheng.vue
+++ haiyblog/src/view/laisheng.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="root">
-        <card/>
+        <div id="left">
+            <card/>
+        </div>
+        <div id="right">
+            <head id="rhhea">
+                <h1>无题</h1>
+            </head>
+            <div id="content">
+            </div>
+        </div>
+        
     </div>
     </template>
     
@@ -18,8 +28,29 @@
         width: 100%;
         height: 100%;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         overflow-y: scroll;
     }
-    
+    #left{
+        
+    }
+    #right{
+        flex: 1;
+       position: sticky;
+       top: 0px;
+       background-color: #262626;
+       border-radius: 20px;
+       margin: 0 20px;
+       padding: 20px;
+       display: flex;
+       flex-direction: column;
+    }
+    #content{
+        padding: 20px;
+        flex: 1;
+    }
+    #rhhea{
+        display: flex;
+        flex-direction: row;
+    }
     </style>
